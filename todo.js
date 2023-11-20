36ba4997424f7a5930789fc7e8d1aee47bfffa06
Mario Bilić <mario.bilic4@gmail.com>
2023-11-20 18:46:36 +0100

    .

--- todo.js
+++ todo.js
@@ -1,67 +1,67 @@
 const todoList = {
-    tasks: [],
+  tasks: [],
 
-    addTask: function(taskText) {
-      this.tasks.push({
-        text: taskText,
-        completed: false
-      });
-      this.render();
-    },
+  addTask: function (taskText) {
+    this.tasks.push({
+      text: taskText,
+      completed: false,
+    });
+    this.render();
+  },
 
-    render: function() {
-        const todoListElement = document.getElementById('todo-list');
-        todoListElement.innerHTML = '';
-      
-        this.tasks.forEach((task, index) => {
-          const listItem = document.createElement('li');
-          const checkbox = document.createElement('input');
-          checkbox.type = 'checkbox';
-          checkbox.checked = task.completed;
-          checkbox.addEventListener('change', () => this.toggleCompleted(index));
-      
-          const taskText = document.createElement('span');
-          taskText.textContent = task.text;
-      
-          const deleteButton = document.createElement('button');
-          deleteButton.className = 'delete-button';
-          deleteButton.textContent = 'Delete';
-          deleteButton.addEventListener('click', () => this.deleteTask(index));
-      
-          listItem.appendChild(checkbox);
-          listItem.appendChild(taskText);
-          listItem.appendChild(deleteButton);
+  render: function () {
+    const todoListElement = document.getElementById("todo-list");
+    todoListElement.innerHTML = "";
 
-          if (task.completed) {
-            taskText.classList.add('todoDone');
-          }
-      
-          todoListElement.appendChild(listItem);
-        });
-      },
+    this.tasks.forEach((task, index) => {
+      const listItem = document.createElement("li");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.checked = task.completed;
+      checkbox.addEventListener("change", () => this.toggleCompleted(index));
 
-    toggleCompleted: function(index) {
-      this.tasks[index].completed = !this.tasks[index].completed;
-      this.render();
-      listItem.classList
-    },
+      const taskText = document.createElement("span");
+      taskText.textContent = task.text;
 
-    deleteTask: function(index) {
-      this.tasks.splice(index, 1);
-      this.render();
-    }
-  };
+      const deleteButton = document.createElement("button");
+      deleteButton.className = "deleteBtn";
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", () => this.deleteTask(index));
 
-  function addTask() {
-    const newTaskInput = document.getElementById('new-task');
-    const newTaskText = newTaskInput.value.trim();
+      listItem.appendChild(checkbox);
+      listItem.appendChild(taskText);
+      listItem.appendChild(deleteButton);
 
-    if (newTaskText !== '') {
-      todoList.addTask(newTaskText);
-      newTaskInput.value = '';
-    }
+      if (task.completed) {
+        taskText.classList.add("todoDone");
+      }
+
+      todoListElement.appendChild(listItem);
+    });
+  },
+
+  toggleCompleted: function (index) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+    this.render();
+    listItem.classList;
+  },
+
+  deleteTask: function (index) {
+    this.tasks.splice(index, 1);
+    this.render();
+  },
+};
+
+function addTask() {
+  const newTaskInput = document.getElementById("new-task");
+  const newTaskText = newTaskInput.value.trim();
+
+  if (newTaskText !== "") {
+    todoList.addTask(newTaskText);
+    newTaskInput.value = "";
   }
+}
 
-  todoList.render();
+todoList.render();
 
-  window.addEventListener("load", todoList.init);
+window.addEventListener("load", todoList.init);
